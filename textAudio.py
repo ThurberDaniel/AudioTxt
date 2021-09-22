7efd4a10b112fe56bce59ff494e9575021e50cb8
ThurberDaniel <phxburg@gmail.com>
2021-09-22 14:59:03 -0600

    init

--- textAudio.py
+++ textAudio.py
@@ -3,6 +3,7 @@ from playsound import playsound
 
 audio = 'talk.mp3'
 language = 'en'
+
 sp = gTTS(text="enter yuor text which you want to convert into audio file",
           lang=language, slow=False)
 sp.save(audio)
