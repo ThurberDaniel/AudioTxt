43656a05efd7a310547bc5f46ae5bc2e53844a14
ThurberDaniel <phxburg@gmail.com>
2021-09-22 20:43:38 -0600

    new

---
+++ Pyulator/calculator.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+print("Select Opertaion")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+
+while True:
+    choice = input("ENTER choice(1/2/3/4):")
