64d7e3df33087b7dd569adef6ca098d2cf5fd9aa
ThurberDaniel <phxburg@gmail.com>
2021-09-23 11:44:56 -0600

    finish

--- Pyulator/calculator.py
+++ Pyulator/calculator.py
@@ -22,3 +22,26 @@ print("4. DIVIDE")
 
 while True:
     choice = input("ENTER choice(1/2/3/4):")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, '/', num2, "=", divide(num1, num2))
+
+        next = input("Let's do next calulation? (yes/no): ")
+        if next == "no":
+            break
+
+    else:
+        print("Invalid Input")
